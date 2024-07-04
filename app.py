9e7560cfaccfd5ee7cb5d9410219921a09122893
Thanawat Potidet <thanawatpotidech@gmail.com>
2024-07-04 21:51:49 +0700

    debug

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import sys
+import logging
 
-import os
-from openai import AzureOpenAI, DefaultHttpxClient
-from dotenv import load_dotenv, find_dotenv
-from azure.core.credentials import AzureKeyCredential  
-from azure.search.documents import SearchClient  
-from azure.search.documents.models import Vector  
-import google.generativeai as genai
+logging.basicConfig(filename='/home/LogFiles/python_error.log', level=logging.ERROR)
 
+try:
+    from flask import Flask, request, abort
+    from linebot import LineBotApi, WebhookHandler
+    from linebot.exceptions import InvalidSignatureError
+    from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+    import os
+    from openai import AzureOpenAI, DefaultHttpxClient
+    from dotenv import load_dotenv, find_dotenv
+    from azure.core.credentials import AzureKeyCredential  
+    from azure.search.documents import SearchClient  
+    from azure.search.documents.models import Vector  
+    import google.generativeai as genai
+except Exception as e:
+    logging.error(f"Startup Error: {str(e)}", exc_info=True)
+    print(f"Error: {str(e)}", file=sys.stderr)
+    raise
 
 # Azure AI Search
 service_endpoint = os.environ['AZURE_AI_SEARCH_ENDPOINT']
